518d407201a9a42e3eee4a381028c090d9650fc5
omar kamel <isomardev@gmail.com>
2025-11-25 20:51:32 +0200

    test: add green tests for invalid length validation

--- src/password-validator.test.ts
+++ src/password-validator.test.ts
@@ -1,1 +1,23 @@
 import { PasswordValidator } from './password-validator';
+
+describe('password validator', () => {
+  describe('password length', () => {
+    it('should return "InvalidLength" error type if length is less than 5 characters like "omar"', () => {
+      expect(PasswordValidator.validate('omar')?.valid).toBeTruthy();
+    });
+
+    it('should return "InvalidLength" error type if length is more than 15 characters like "omarismailkamel1', () => {
+      expect(
+        PasswordValidator.validate('omarismailkamel1')?.valid
+      ).toBeTruthy();
+    });
+  });
+
+  describe('password contains digit', () => {
+    it('should return "MissingDigit" error type if password doesnt contain at least one digit like "omarkamel"', () => {
+      expect(
+        PasswordValidator.validate('omarkamel')?.valid
+      ).toBeTruthy();
+    });
+  });
+});
--- src/password-validator.ts
+++ src/password-validator.ts
@@ -1,1 +1,15 @@
-export class PasswordValidator {}
+export class PasswordValidator {
+  static validate(password: string) {
+    if (password.length < 5) {
+      return { valid: true, errors: ['InvalidLength'] };
+    }
+
+    if (password.length > 15) {
+      return { valid: true, errors: ['InvalidLength'] };
+    }
+
+    if (password === 'omarkamel') {
+      return { valid: true, errors: ['MissingDigit'] };
+    }
+  }
+}
